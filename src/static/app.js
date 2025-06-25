6a5598a7ea99c23c56e9241863d321027093a982
JonathansmDev <jonathansm.dev@gmail.com>
2025-06-25 14:40:44 +0000

    Adiciona seção de participantes nos cards de atividade

--- src/static/app.js
+++ src/static/app.js
@@ -20,11 +20,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const spotsLeft = details.max_participants - details.participants.length;
 
+        // Monta a lista de participantes
+        let participantsSection = '';
+        if (details.participants && details.participants.length > 0) {
+          participantsSection = `
+            <div class="participants">
+              <h5>Participantes inscritos:</h5>
+              <ul>
+                ${details.participants.map(p => `<li>${p}</li>`).join('')}
+              </ul>
+            </div>
+          `;
+        } else {
+          participantsSection = `
+            <div class="participants">
+              <h5>Participantes inscritos:</h5>
+              <ul><li><em>Nenhum inscrito ainda</em></li></ul>
+            </div>
+          `;
+        }
+
         activityCard.innerHTML = `
           <h4>${name}</h4>
           <p>${details.description}</p>
           <p><strong>Agenda:</strong> ${details.schedule}</p>
           <p><strong>Disponibilidade:</strong> ${spotsLeft} vagas disponíveis</p>
+          ${participantsSection}
         `;
 
         activitiesList.appendChild(activityCard);
